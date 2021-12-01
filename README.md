85e85e74232d82f98444d70f926ead98369fd81d
MarsRon <71264052+MarsRon@users.noreply.github.com>
2021-12-01 23:15:34 +0800

    Day 1

---
+++ README.md
@@ -0,0 +1,7 @@
+# Advent of Code 2021
+
+My Node.js solutions to the Advent of Code 2021 puzzles.
+
+If you don't wanna get spoiled, try the puzzles at https://adventofcode.com/2021.
+
+Personally I don't think anyone would understand my code because I can't write readable code 🎉
---
+++ day1/index.js
@@ -0,0 +1,55 @@
+// Day 1
+// https://adventofcode.com/2021/leaderboard/day/1
+
+const { readFileSync } = require('fs')
+const path = require('path')
+
+// Read input file
+// split by newlines
+// Turn string into numbers
+const inputFilePath = path.join(__dirname, 'input.txt')
+const input = readFileSync(inputFilePath)
+  .toString()
+  .split('\n')
+  .map(string => Number(string))
+
+// Part 1
+
+const largerThanPrevious = []
+
+// i starts from 1 to omit first measurement, start from second
+for (let i = 1; i < input.length; i++) {
+  // If current is larger than previous, count it
+  if (input[i] > input[i - 1]) {
+    largerThanPrevious.push(input[i])
+  }
+}
+
+console.log(`Part 1: ${largerThanPrevious.length}`)
+
+// Part 2
+
+// All sums
+const sums = []
+
+for (let i = 0; i < input.length; i++) {
+  // Break if not enough measurement to sum
+  if (i + 2 >= input.length) break
+
+  // Get i, i+1, i+2
+  const sum = input[i] + input[i + 1] + input[i + 2]
+  sums.push(sum)
+}
+
+// Increased sum
+const largerThanPreviousSum = []
+
+// i starts from 1 to omit first measurement, start from second
+for (let i = 1; i < sums.length; i++) {
+  // If current is larger than previous, count it
+  if (sums[i] > sums[i - 1]) {
+    largerThanPreviousSum.push(sums[i])
+  }
+}
+
+console.log(`Part 2: ${largerThanPreviousSum.length}`)
