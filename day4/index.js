f522792657979d02603682c13a57e74d1dcc0768
MarsRon <71264052+MarsRon@users.noreply.github.com>
2021-12-04 19:24:32 +0800

    Add docs, add perf timing, no JSON.parse, reset numbers

--- day4/index.js
+++ day4/index.js
@@ -3,6 +3,9 @@
 
 const { readFileSync } = require('fs')
 const path = require('path')
+const { performance } = require('perf_hooks')
+
+const t0 = performance.now()
 
 // Read input file
 // Split by newlines
@@ -11,9 +14,12 @@ const input = readFileSync(inputFilePath, 'utf8')
   .trim() // remove final newline
   .split('\n\n')
 
-// first line is the numbers to draw
-// use hacky JSON.parse, turn into array
-const draws = JSON.parse(`[${input.shift()}]`)
+// First line is the numbers to draw, so .shift()
+// Then we split and turn into numbers
+const draws = input
+  .shift()
+  .split(',')
+  .map(s => Number(s))
 
 // A board is something like this
 /*
@@ -39,6 +45,7 @@ const boards = input.map(string =>
 
 // Part 1
 
+// Mutate the marked value of the number, shouldn't've done this smh
 const markNumber = (board, target) => {
   for (const column of board) {
     const number = column.find(x => x.value === target && !x.marked)
@@ -50,19 +57,23 @@ const markNumber = (board, target) => {
 }
 
 const checkForWin = board => {
+  // Check horizontal
   for (const column of board) {
     if (column.every(x => x.marked)) {
       return true
     }
   }
+  // Check vertical
   for (let i = 0; i < 5; i++) {
     if (board.every(column => column[i].marked)) {
       return true
     }
   }
+
   return false
 }
 
+// Yeah get the first winning board
 const getWinningBoard = (boards, draws) => {
   for (const draw of draws) {
     for (const board of boards) {
@@ -74,10 +85,13 @@ const getWinningBoard = (boards, draws) => {
   }
 }
 
+// Scoping this because part 2 uses similar variables im too lazy to rename
 {
   const { board, number } = getWinningBoard(boards, draws)
+  // Sum up unmarked numbers, ignore ugly reduce code
   const unmarkedNumbers = board.reduce(
-    (acc, cur) => acc + cur.reduce((a, c) => (!c.marked ? a + c.value : a), 0),
+    (sum, column) =>
+      sum + column.reduce((acc, n) => (!n.marked ? acc + n.value : acc), 0),
     0
   )
   console.log(`Part 1: ${number.value * unmarkedNumbers}`)
@@ -85,25 +99,44 @@ const getWinningBoard = (boards, draws) => {
 
 // Part 2
 
+// I forgot to unmark/reset the numbers fml
+// 3 for loops, nice.
+for (const board of boards) {
+  for (const column of board) {
+    for (const number of column) {
+      number.marked = false
+    }
+  }
+}
+
+// Remove boards that win first until last one wins
 const getLastWinningBoard = (boards, draws) => {
-  boards = [...boards]
   for (const draw of draws) {
     let number
     for (const board of boards) {
       number = markNumber(board, draw)
     }
+    // If it's the last board and has win
     if (boards.length === 1 && checkForWin(boards[0])) {
       return { board: boards[0], number }
     }
+    // Filter out boards with wins
     boards = boards.filter(board => !checkForWin(board))
   }
 }
 
+// Since part 1 is scoped, why not scope this too
 {
   const { board, number } = getLastWinningBoard(boards, draws)
+  // Sum up unmarked numbers, ignore ugly reduce code
   const unmarkedNumbers = board.reduce(
-    (acc, cur) => acc + cur.reduce((a, c) => (!c.marked ? a + c.value : a), 0),
+    (sum, column) =>
+      sum + column.reduce((acc, n) => (!n.marked ? acc + n.value : acc), 0),
     0
   )
   console.log(`Part 2: ${number.value * unmarkedNumbers}`)
 }
+
+const t1 = performance.now()
+
+console.log(`${(t1 - t0).toLocaleString()}ms`)
